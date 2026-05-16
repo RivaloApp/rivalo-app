94d100eea7dc21598c9406a2884dcf437bd95ae6
RivaloApp <silvyrizzello@gmail.com>
2026-05-16 23:41:01 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,28 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
+import { Orbitron } from "next/font/google";
+
+const orbitron = Orbitron({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700", "800", "900"],
+});
+
 export const metadata: Metadata = {
   title: "Rivalo",
-  description: "La rivalità sportiva inizia qui."
+  description: "La rivalità sportiva inizia qui.",
 };
 
 export default function RootLayout({
-  children
+  children,
 }: {
   children: React.ReactNode;
 }) {
   return (
     <html lang="it">
-      <body>{children}</body>
+      <body className={orbitron.className}>
+        {children}
+      </body>
     </html>
   );
 }
